22d13e3e88f04de8abcb5cdc0dd603368f939561
marshl <marshl@live.com.au>
2019-11-19 21:14:02 +1100

    Blackened

--- bimile.py
+++ bimile.py
@@ -8,26 +8,36 @@ import queue
 
 
 class TrafficModel:
-    DOWN = 'D'
-    RIGHT = 'R'
-    EMPTY = '.'
+    DOWN = "D"
+    RIGHT = "R"
+    EMPTY = "."
 
     def __init__(self, scale: int, density: float):
         self.scale = scale
         self.density = density
         self.turn = self.DOWN
 
-        self.cells = [[
-            self.EMPTY if random() > density
-            else self.DOWN if random() >= 0.5
-            else self.RIGHT
-            for x in range(self.scale)
-        ] for y in range(self.scale)]
+        self.cells = [
+            [
+                self.EMPTY
+                if random() > density
+                else self.DOWN
+                if random() >= 0.5
+                else self.RIGHT
+                for x in range(self.scale)
+            ]
+            for y in range(self.scale)
+        ]
 
         self.cell_history = [self.cells]
 
     def __str__(self):
-        return "\n".join([' '.join([str(self.cells[y][x]) for x in range(self.scale)]) for y in range(self.scale)])
+        return "\n".join(
+            [
+                " ".join([str(self.cells[y][x]) for x in range(self.scale)])
+                for y in range(self.scale)
+            ]
+        )
 
     def step(self):
 
@@ -40,13 +50,19 @@ class TrafficModel:
                     continue
 
                 if self.turn == self.DOWN:
-                    if self.cells[y][x] == self.DOWN and self.cells[(y + 1) % self.scale][x] == self.EMPTY:
+                    if (
+                        self.cells[y][x] == self.DOWN
+                        and self.cells[(y + 1) % self.scale][x] == self.EMPTY
+                    ):
                         cell_copy[(y + 1) % self.scale][x] = self.DOWN
                     else:
                         cell_copy[y][x] = self.cells[y][x]
 
                 elif self.turn == self.RIGHT:
-                    if self.cells[y][x] == self.RIGHT and self.cells[y][(x + 1) % self.scale] == self.EMPTY:
+                    if (
+                        self.cells[y][x] == self.RIGHT
+                        and self.cells[y][(x + 1) % self.scale] == self.EMPTY
+                    ):
                         cell_copy[y][(x + 1) % self.scale] = self.RIGHT
                     else:
                         cell_copy[y][x] = self.cells[y][x]
@@ -59,14 +75,16 @@ class TrafficModel:
 
 
 def render_frame(frame_index: int):
-    print(f"\rFrame {frame_index}", end='')
+    print(f"\rFrame {frame_index}", end="")
 
-    frame = Image(width=traffic_model.scale * args.cell_size,
-                  height=traffic_model.scale * args.cell_size,
-                  background=Color('white'))
+    frame = Image(
+        width=traffic_model.scale * args.cell_size,
+        height=traffic_model.scale * args.cell_size,
+        background=Color("white"),
+    )
 
     with Drawing() as draw:
-        draw.fill_color = Color('#0000ff')
+        draw.fill_color = Color("#0000ff")
 
         for y in range(traffic_model.scale):
             for x in range(traffic_model.scale):
@@ -74,18 +92,26 @@ def render_frame(frame_index: int):
                     if args.cell_size == 1:
                         draw.point(x, y)
                     else:
-                        draw.rectangle(left=x * args.cell_size, top=y * args.cell_size,
-                                       width=args.cell_size - 1, height=args.cell_size - 1)
+                        draw.rectangle(
+                            left=x * args.cell_size,
+                            top=y * args.cell_size,
+                            width=args.cell_size - 1,
+                            height=args.cell_size - 1,
+                        )
 
-        draw.fill_color = Color('#ff0000')
+        draw.fill_color = Color("#ff0000")
         for y in range(traffic_model.scale):
             for x in range(traffic_model.scale):
                 if traffic_model.cell_history[frame_index][y][x] == TrafficModel.RIGHT:
                     if args.cell_size == 1:
                         draw.point(x, y)
                     else:
-                        draw.rectangle(left=x * args.cell_size, top=y * args.cell_size,
-                                       width=args.cell_size - 1, height=args.cell_size - 1)
+                        draw.rectangle(
+                            left=x * args.cell_size,
+                            top=y * args.cell_size,
+                            width=args.cell_size - 1,
+                            height=args.cell_size - 1,
+                        )
         draw(frame)
 
     frames[frame_index] = frame
@@ -104,21 +130,29 @@ class RenderThread(threading.Thread):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Creates a Biham–Middleton–Levine traffic model gif')
-    parser.add_argument('scale', type=int,
-                        help='the height and width of the grid')
+    parser = argparse.ArgumentParser(
+        description="Creates a Biham–Middleton–Levine traffic model gif"
+    )
+    parser.add_argument("scale", type=int, help="the height and width of the grid")
 
-    parser.add_argument('density', type=float,
-                        help='the density of the traffic (0.0 - 1.0)')
+    parser.add_argument(
+        "density", type=float, help="the density of the traffic (0.0 - 1.0)"
+    )
 
-    parser.add_argument('frame_count', type=int,
-                        help='the number of frames in the output gif')
+    parser.add_argument(
+        "frame_count", type=int, help="the number of frames in the output gif"
+    )
 
-    parser.add_argument('--frame-skip', type=int, default=1,
-                        help='the number of spaces that the cells should move in each frame')
+    parser.add_argument(
+        "--frame-skip",
+        type=int,
+        default=1,
+        help="the number of spaces that the cells should move in each frame",
+    )
 
-    parser.add_argument('--cell-size', type=int, default=1,
-                        help='the number of pixels for each cell')
+    parser.add_argument(
+        "--cell-size", type=int, default=1, help="the number of pixels for each cell"
+    )
 
     args = parser.parse_args()
 
@@ -130,7 +164,7 @@ if __name__ == "__main__":
             traffic_model.step()
 
         traffic_model.save_state()
-        print(f"\rSimulation step {i}", end='')
+        print(f"\rSimulation step {i}", end="")
 
     print("\nSimulation complete. Rendering...")
     frames = [None] * args.frame_count
@@ -151,5 +185,5 @@ if __name__ == "__main__":
             img.sequence.append(frame)
             frame.delay = 10
 
-        img.type = 'optimize'
-        img.save(filename='output.gif')
+        img.type = "optimize"
+        img.save(filename="output.gif")
